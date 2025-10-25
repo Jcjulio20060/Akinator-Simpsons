a9e78adc5868654bff0946bcd61cb86394b0b545
Jcjulio20060 <juliocesarqueiroz20060@gmail.com>
2025-10-25 13:19:49 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Akinator-Simpsons',
+  webDir: 'www'
+};
+
+export default config;
